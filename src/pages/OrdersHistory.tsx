7f3df20b23da66e1fbf41221826e22308f1b647f
Narek2481 <narekengibaryan.1@mail.ru>
2023-12-29 16:46:46 +0400

    last commit

--- src/pages/OrdersHistory.tsx
+++ src/pages/OrdersHistory.tsx
@@ -1,40 +1,33 @@
 import {useGetOrders} from "../common/hooks/useGetOrders";
 import {OrdersCard} from "../components/orders/OrdersCard";
-import {Fragment, memo} from "react";
+import {Fragment, memo, useEffect, useState} from "react";
 import "../styles/order-history/OrderHistory.scss"
 
 export const OrdersHistory = memo(() => {
+    const [dataForMap, setDataForMap] = useState<any[]>([])
     const {data} = useGetOrders()
+    useEffect(() => {
+        let arr:any[] = []
+        if (data){
+            data.forEach(elem => {
+                console.log(elem.orderItems)
+               arr =  arr.concat(elem.orderItems)
+            })
+        }
+        setDataForMap(arr)
+    }, [data]);
 
     return (
         <div className="orderHistory">
             {
-                !!data && data?.map(elem => {
+                dataForMap.map(elem => {
                     return (
                         <Fragment key={elem._id}>
-                            {elem.orderItems[0] ? <OrdersCard product={elem.orderItems[0]?.product}/> : <></>}
+                            <OrdersCard product={elem.product}/>
                         </Fragment>
                     )
                 })
             }
-            {/*{*/}
-            {/*    !!data && data.map(elem => {*/}
-            {/*        // @ts-ignore*/}
-            {/*        return <Fragment key={elem.id}>*/}
-            {/*            {*/}
-            {/*                !!elem.orderItems[0].product.image && <UniversalProducts text={} img={elem.orderItems[0].product.image as string}/>*/}
-            {/*            }*/}
-            {/*        </Fragment>*/}
-            {/*    })*/}
-            {/*}*/}
-            {/*{*/}
-            {/*   !!data && data.map((datum,i) => (*/}
-            {/*        <div key={i}>*/}
-            {/*            {`${datum.orderItems[0].product}`}*/}
-            {/*        </div>*/}
-            {/*    ))*/}
-            {/*}*/}
-            {/*{data && JSON.stringify(data[0].orderItems[0].product,null,5)}*/}
         </div>
     );
 });
